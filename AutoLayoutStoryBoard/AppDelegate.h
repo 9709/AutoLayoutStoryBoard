d1d28a68c06ec5ff1e2d592a08049a60afa27770
Matthew Chan <mat_t4@hotmail.com>
2019-01-15 17:05:10 -0800

    Initial Commit

---
+++ AutoLayoutStoryBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  AutoLayoutStoryBoard
+//
+//  Created by Matthew Chan on 2019-01-15.
+//  Copyright © 2019 Matthew Chan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
